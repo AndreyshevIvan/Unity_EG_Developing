8b26d037e7b18ba11cca1de13f24c63fc3f4ff3a
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-03-08 20:40:25 +0300

    Убраны свой код сцен

---
+++ Arkanoid/Assets/Scripts/ButtonCommands.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonCommands : MonoBehaviour
+{
+
+    public void SetGameplayScene()
+    {
+        SceneManager.LoadScene("Scenes/Gameplay");
+    }
+    public void SetMenuScene()
+    {
+        SceneManager.LoadScene("Scenes/Menu");
+    }
+    public void SetGameoverScene()
+    {
+        SceneManager.LoadScene("Scenes/Gameover");
+    }
+    public void SetChangeLevelScene()
+    {
+        SceneManager.LoadScene("Scenes/ChangeLevel");
+    }
+    public void SetExitScene()
+    {
+        SceneManager.LoadScene("Scenes/Exit");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
